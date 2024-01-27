8955183b6d264dc3252d62af3899b9fa3c949dfc
Gabriel Frahm <gabriel_frahm@hotmail.com>
2024-01-27 17:46:16 -0300

    feat(wip): validation

--- domain/src/test/java/org/example/domain/category/CategoryTest.java
+++ domain/src/test/java/org/example/domain/category/CategoryTest.java
@@ -23,5 +23,23 @@ public class CategoryTest {
        Assertions.assertNotNull(actualCategory.getCreatedAt());
        Assertions.assertNotNull(actualCategory.getUpdatedAt());
        Assertions.assertNull(actualCategory.getDeletedAt());
-    }
+   }
+
+   @Test
+   public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError(){
+       final String expectedName = null;
+       final var expectedErrorMessage = "'name' should not be null";
+       final var expectedErrorCount = 1;
+       final var expectedDescription = "the most category watched";
+       final var expectedIsActive = true;
+
+       final var actualCategory =
+               Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+       final var actualException =
+               Assertions.assertThrows(DomainException.class, () -> actualCategory.validate());
+
+       Assertions.assertEquals(expectedErrorCount, actualCategory.getErrors().size());
+       Assertions.assertEquals(expectedErrorMessage, actualCategory.getErrors().get(0));
+   }
 }
